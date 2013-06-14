bd155b839274df6704eeff00f16e5f1d02bcaba4
Pradeep Murukannaiah <pradeepmcl@gmail.com>
2013-06-14 15:07:17 -0400

    Intermediate commit.

---
+++ src/edu/ncsu/mas/platys/android/ui/AppsFragment.java
@@ -0,0 +1,7 @@
+package edu.ncsu.mas.platys.android.ui;
+
+import android.app.Fragment;
+
+public class AppsFragment extends Fragment {
+
+}
--- src/edu/ncsu/mas/platys/android/ui/PlatysActivity.java
+++ src/edu/ncsu/mas/platys/android/ui/PlatysActivity.java
@@ -1,20 +1,81 @@
 package edu.ncsu.mas.platys.android.ui;
 
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.Toast;
 import edu.ncsu.mas.platys.android.R;
 
 public class PlatysActivity extends Activity {
-  private final String mServerMode = null;
-  private final String mUserName = null;
+
+  public static final String PLATYS_PREFS = "platys_prefs";
+
+  public static final String PREFS_KEY_SERVER_MODE = "server_mode";
+  public static final String PREFS_KEY_USERNAME = "username";
+
+  private SharedPreferences mPreferences;
+
+  private ActionBar mActionBar;
+
+  private ServerModeChooserActivity.ServerMode mServerMode;
+  private String mUsername;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    startActivity(new Intent(this, ServerModeChooserActivity.class));
+    mPreferences = getSharedPreferences(PLATYS_PREFS, Context.MODE_PRIVATE);
+
+    try {
+      mServerMode = ServerModeChooserActivity.ServerMode.valueOf(mPreferences.getString(
+          PREFS_KEY_SERVER_MODE, ""));
+      mUsername = mPreferences.getString(PREFS_KEY_USERNAME, "");
+    } catch (IllegalArgumentException e) {
+      mServerMode = null;
+    }
+
+    if (mServerMode == null || mUsername.length() == 0) {
+      startActivity(new Intent(this, ServerModeChooserActivity.class));
+    }
+    
+    mActionBar = getActionBar();    
+    mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+    mActionBar.setTitle(getString(R.string.app_name));
+    mActionBar.setSubtitle(mUsername);
+    
+    mActionBar.addTab(mActionBar.newTab().setText("Places")
+        .setTabListener(new TabListener<PlacesFragment>(this, "places", PlacesFragment.class)));
+    
+    mActionBar.addTab(mActionBar.newTab().setText("Sensors")
+        .setTabListener(new TabListener<SensorsFragment>(this, "sensors", SensorsFragment.class)));
+    
+    mActionBar.addTab(mActionBar.newTab().setText("Apps")
+        .setTabListener(new TabListener<AppsFragment>(this, "apps", AppsFragment.class)));
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+
+    try {
+      mServerMode = ServerModeChooserActivity.ServerMode.valueOf(mPreferences.getString(
+          PREFS_KEY_SERVER_MODE, ""));
+      mUsername = mPreferences.getString(PREFS_KEY_USERNAME, "");
+    } catch (IllegalArgumentException e) {
+      mServerMode = null;
+    }
+
+    if (mServerMode == null || mUsername.length() == 0) {
+      startActivity(new Intent(this, ServerModeChooserActivity.class));
+    }
   }
 
   @Override
@@ -24,4 +85,48 @@ public class PlatysActivity extends Activity {
     return true;
   }
 
+  public static class TabListener<T extends Fragment> implements ActionBar.TabListener {
+    private final Activity mActivity;
+    private final String mTag;
+    private final Class<T> mClass;
+    private final Bundle mArgs;
+    private Fragment mFragment;
+
+    public TabListener(Activity activity, String tag, Class<T> clz) {
+      this(activity, tag, clz, null);
+    }
+
+    public TabListener(Activity activity, String tag, Class<T> clz, Bundle args) {
+      mActivity = activity;
+      mTag = tag;
+      mClass = clz;
+      mArgs = args;
+
+      mFragment = mActivity.getFragmentManager().findFragmentByTag(mTag);
+      if (mFragment != null && !mFragment.isDetached()) {
+        FragmentTransaction ft = mActivity.getFragmentManager().beginTransaction();
+        ft.detach(mFragment);
+        ft.commit();
+      }
+    }
+
+    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+      if (mFragment == null) {
+        mFragment = Fragment.instantiate(mActivity, mClass.getName(), mArgs);
+        ft.add(android.R.id.content, mFragment, mTag);
+      } else {
+        ft.attach(mFragment);
+      }
+    }
+
+    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+      if (mFragment != null) {
+        ft.detach(mFragment);
+      }
+    }
+
+    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+      Toast.makeText(mActivity, "Reselected!", Toast.LENGTH_SHORT).show();
+    }
+  }
 }
